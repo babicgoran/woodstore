4f0bc3f25f284e2a17c23dd4a2a19af9a8f52dde
unknown <babic.goran@outlook.com>
2020-05-15 23:35:53 +0200

    landing added, waiting responsive

---
+++ header.js
@@ -0,0 +1,28 @@
+document.write ('\
+<nav class ="navbar navbar-expand-lg navbar-light col-xl-10">\
+    <a class="navbar-brand" href="#">Beodrvo Company</a>\
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"\
+        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">\
+        <span class="navbar-toggler-icon"></span>\
+    </button>\
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">\
+        <ul class="navbar-nav">\
+            <li class="nav-item active">\
+                <a class="nav-link" href="#">Početna<span class="sr-only">(current)</span></a>\
+            </li>\
+            <li class="nav-item">\
+                <a class="nav-link" href="#">Hrastove Ploče</a>\
+            </li>\
+            <li class="nav-item">\
+                <a class="nav-link" href="#">Gazišta</a>\
+            </li>\
+            <li class="nav-item">\
+                <a class="nav-link" href="#">Lamelirani elementi</a>\
+            </li>\
+            <li class="nav-item">\
+                <a class="nav-link" href="#">Kontakt</a>\
+            </li>\
+        </ul>\
+    </div>\
+</nav>\
+')
