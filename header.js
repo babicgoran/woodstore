0df0c9b9fd3e0cf6f32de3e363a4669189b3c1ad
unknown <babic.goran@outlook.com>
2020-05-16 22:15:58 +0200

    page stolovi added

--- header.js
+++ header.js
@@ -1,6 +1,6 @@
-document.write ('\
+document.write('\
 <nav class ="navbar navbar-expand-lg navbar-light col-xl-10">\
-    <a class="navbar-brand" href="#">Beodrvo Company</a>\
+    <a class="navbar-brand" href="apsolutna putanja na naslovnu">Beodrvo Company</a>\
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"\
         aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">\
         <span class="navbar-toggler-icon"></span>\
@@ -8,19 +8,22 @@ document.write ('\
     <div class="collapse navbar-collapse" id="navbarNavDropdown">\
         <ul class="navbar-nav">\
             <li class="nav-item active">\
-                <a class="nav-link" href="#">Početna<span class="sr-only">(current)</span></a>\
+                <a class="nav-link" href="apsolutna putanja na naslovnu">Početna<span class="sr-only">(current)</span></a>\
             </li>\
             <li class="nav-item">\
-                <a class="nav-link" href="#">Hrastove Ploče</a>\
+                <a class="nav-link" href="apsolutna putanja na hrastove ploce">Hrastove Ploče</a>\
             </li>\
             <li class="nav-item">\
-                <a class="nav-link" href="#">Gazišta</a>\
+                <a class="nav-link" href="apsolutna putanja na gazista">Gazišta</a>\
             </li>\
             <li class="nav-item">\
-                <a class="nav-link" href="#">Lamelirani elementi</a>\
+                <a class="nav-link" href="apsolutna putanja na lamelirane elemente">Lamelirani elementi</a>\
             </li>\
             <li class="nav-item">\
-                <a class="nav-link" href="#">Kontakt</a>\
+                <a class="nav-link" href="apsolutna putanja na stolove">Stolovi</a>\
+            </li>\
+            <li class="nav-item">\
+                <a class="nav-link" href="apsolutna putanja na kontakt">Kontakt</a>\
             </li>\
         </ul>\
     </div>\
