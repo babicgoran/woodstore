c8cdfcbd3d3c6ee3c5eedb377b99fc35fc5c23e6
unknown <babic.goran@outlook.com>
2020-05-22 00:03:55 +0200

    problemi sa fotkama na serveru

--- header.js
+++ header.js
@@ -1,6 +1,6 @@
 document.write('\
 <nav class ="navbar navbar-expand-lg navbar-light col-xl-10">\
-    <a class="navbar-brand" href="apsolutna putanja na naslovnu"><img src="/images/logo.jpg" width="100px" alt="logo"></a>\
+    <a class="navbar-brand" href="http://www.hrastoveploce.com/"><img src="/images/logo.jpg" width="100px" alt="logo"></a>\
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"\
         aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">\
         <span class="navbar-toggler-icon"></span>\
@@ -8,22 +8,22 @@ document.write('\
     <div class="collapse navbar-collapse" id="navbarNavDropdown">\
         <ul class="navbar-nav">\
             <li class="nav-item active">\
-                <a class="nav-link" href="apsolutna putanja na naslovnu">Početna<span class="sr-only">(current)</span></a>\
+                <a class="nav-link" href="http://www.hrastoveploce.com/">Početna<span class="sr-only">(current)</span></a>\
             </li>\
             <li class="nav-item">\
-                <a class="nav-link" href="./hrastoveploce/index.html">Hrastove Ploče</a>\
+                <a class="nav-link" href="http://www.hrastoveploce.com/hrastoveploce/index.html">Hrastove Ploče</a>\
             </li>\
             <li class="nav-item">\
-                <a class="nav-link" href="./gazista/index.html">Gazišta</a>\
+                <a class="nav-link" href="http://www.hrastoveploce.com/gazista/index.html">Gazišta</a>\
             </li>\
             <li class="nav-item">\
-                <a class="nav-link" href="apsolutna putanja na lamelirane elemente">Lamelirani elementi</a>\
+                <a class="nav-link" href="http://www.hrastoveploce.com/lameliranielementi/index.html">Lamelirani elementi</a>\
             </li>\
             <li class="nav-item">\
-                <a class="nav-link" href="apsolutna putanja na stolove">Stolovi</a>\
+                <a class="nav-link" href="http://www.hrastoveploce.com/stolovi/index.html">Stolovi</a>\
             </li>\
             <li class="nav-item">\
-                <a class="nav-link" href="apsolutna putanja na kontakt">Kontakt</a>\
+                <a class="nav-link" href="http://www.hrastoveploce.com/kontakt/index.html">Kontakt</a>\
             </li>\
         </ul>\
     </div>\
