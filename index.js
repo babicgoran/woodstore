1bd787621c42f070a97f43004cb0e047ebc23d16
unknown <milan.491989@yahoo.com>
2020-06-15 01:40:36 +0200

    calculation edited

--- index.js
+++ index.js
@@ -21,34 +21,35 @@
     const getType = (a, b) => {
 
         const prices = {
-            lowerPrice: 0, // cena za plocu duzine manje od 2m 
-            higherPrice: 0 // cena za plocu duzine vece od 2m
+            basePrice:0, //osnovna cena za ploce do 2m 
+            // lowerPrice: 0, // cena za plocu duzine manje od 2m 
+            // higherPrice: 0 // cena za plocu duzine vece od 2m
         }
 
         switch (a + "|" + b) {
             case "20|AB":
-
-                prices.lowerPrice = 80;
-                prices.higherPrice = 96;
+                    prices.basePrice= 80;
+                // prices.lowerPrice = 80;
+                // prices.higherPrice = 96;
 
                 break;
             case "20|BC":
-
-                prices.lowerPrice = 54;
-                prices.higherPrice = 64.8;
+                    prices.basePrice= 54;
+                // prices.lowerPrice = 54;
+                // prices.higherPrice = 64.8;
 
                 break;
 
             case "40|AB":
-
-                prices.lowerPrice = 160;
-                prices.higherPrice = 192;
+                    prices.basePrice= 160;
+                // prices.lowerPrice = 160;
+                // prices.higherPrice = 192;
 
                 break;
             case "40|BC":
-
-                prices.lowerPrice = 108;
-                prices.higherPrice = 129.6;
+                    prices.basePrice= 108;
+                // prices.lowerPrice = 108;
+                // prices.higherPrice = 129.6;
 
                 break;
             default:
@@ -62,7 +63,8 @@
     const priceCalculation = () => {
 
         const { x, y, z, klasa, premaz } = getInputValues()
-        const { lowerPrice, higherPrice } = getType(z, klasa)
+        // const { lowerPrice, higherPrice } = getType(z, klasa)
+        const { basePrice } = getType(z, klasa)
 
         let length = x / 100
         let width = y / 100
@@ -71,13 +73,32 @@
         let surfaceArea = length * width
         let sideSurface = height * (length + width) // ukupna povrsina bocnih strana 
 
-        length > 2 || width > 2 ?
+        const calcForDimensions = (l, price) => {
 
-            cena.innerHTML = (Math.floor((surfaceArea * (higherPrice + parseInt(premaz)) + sideSurface * parseInt(premaz)) * 100) / 100) + " evra"
+            return (price + l*price)
 
-            :
+        }
+        // length > 2 || width > 2 ?
 
-            cena.innerHTML = (Math.floor((surfaceArea * (lowerPrice + parseInt(premaz)) + sideSurface * parseInt(premaz)) * 100) / 100) + " evra"
+        //     cena.innerHTML = (Math.floor((surfaceArea * (higherPrice + parseInt(premaz)) + sideSurface * parseInt(premaz)) * 100) / 100) + " evra"
+
+        //     :
+
+        //     cena.innerHTML = (Math.floor((surfaceArea * (lowerPrice + parseInt(premaz)) + sideSurface * parseInt(premaz)) * 100) / 100) + " evra"
+
+        if( length < 2 || width < 2 ){
+            cena.innerHTML = (Math.floor((surfaceArea * (basePrice + parseInt(premaz)) + sideSurface * parseInt(premaz)) * 100) / 100) + " evra"
+        }else if((length >= 2 || width >= 2) && (length < 3 && width < 3) ){
+            cena.innerHTML = (Math.floor((surfaceArea * (calcForDimensions(0.2 , basePrice) + parseInt(premaz)) + sideSurface * parseInt(premaz)) * 100) / 100) + " evra"
+        }else if((length >= 3 || width >= 3) && (length < 4 && width < 4 )) {
+            cena.innerHTML = (Math.floor((surfaceArea * (calcForDimensions(0.3 , basePrice) + parseInt(premaz)) + sideSurface * parseInt(premaz)) * 100) / 100) + " evra"
+        }else if ((length >= 4 || width >= 4) && (length < 5 && width < 5 )){
+            cena.innerHTML = (Math.floor((surfaceArea * (calcForDimensions(0.4 , basePrice) + parseInt(premaz)) + sideSurface * parseInt(premaz)) * 100) / 100) + " evra"
+        }else if (length === 5 || width === 5){
+            cena.innerHTML = (Math.floor((surfaceArea * (calcForDimensions(0.5 , basePrice) + parseInt(premaz)) + sideSurface * parseInt(premaz)) * 100) / 100) + " evra"
+        }else{
+            cena.innerHTML="max 5m"
+        }
 
     }
 
